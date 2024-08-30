0ec770b5210c3c3787ff603467a44e53ac767522
Adrien DEBADIER <adriendebadier@hotmail.com>
2024-08-30 18:22:27 +0200

    fix errors from downlaod_fil.c

--- srcs/download_file.c
+++ srcs/download_file.c
@@ -1,48 +1,66 @@
 #include "srcs.h"
 #include "tools.h"
+#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 
 struct host_data	*get_hostdata(char *url)
 {
-	struct host_data	server_data;
-	char				*strs;
-	int					start;
-	int					end;
+	struct host_data	*server_data;
 
 	if (!does_match_with_pattern(url, URL_REGEX)) {
 		fprintf(stderr, "Error : not an url (%s)\n", url);
 		return NULL;
 	}
-	server_data.hostname = "pbs.twimg.com";
-	server_data.filename = "EMtmPFLWkAA8CIS.jpg";
-	server_data.filepath = "/media/EMtmPFLWkAA8CIS.jpg";
-	return &server_data;
+	server_data = malloc(sizeof(struct host_data));
+	if (!server_data) {
+		perror("Memory allocation failed");
+		return NULL;
+	}
+	server_data->hostname = strdup("pbs.twimg.com");
+	server_data->filename = strdup("EMtmPFLWkAA8CIS.jpg");
+	server_data->filepath = strdup("/media/EMtmPFLWkAA8CIS.jpg");
+	if (!server_data->hostname ||
+			!server_data->filename || !server_data->hostname) {
+		free(server_data->hostname);
+		free(server_data->filename);
+		free(server_data->filepath);
+		free(server_data);
+		return NULL;
+	}
+	return server_data;
 }
 
 struct sockaddr	*get_server_socket_address(char *hostname)
 {
 	struct hostent		*host;
-	struct sockaddr_in	server;
+	struct sockaddr_in	*server;
 
 	host = gethostbyname(hostname);
 	if (!host) {
 		fprintf(stderr, "Error : Host not found (%s)\n", hostname);
 		return NULL;
 	}
-	server.sin_addr = *(struct in_addr *)host->h_addr_list[0];
+	server = malloc(sizeof(struct sockaddr_in));
+	if (!server) {
+		perror("Failed to allocate memory for strings");
+		return NULL;
+	}
+	server->sin_addr = *(struct in_addr *)host->h_addr_list[0];
 	// use the host first IP address
-	server.sin_family = AF_INET; // define type of IP (IPv4)
-	server.sin_port = htons(80); // default port for HTTP
-	return (struct sockaddr *)&server;
+	server->sin_family = AF_INET; // define type of IP (IPv4)
+	server->sin_port = htons(80); // default port for HTTP
+	return (struct sockaddr *)server;
 }
 
 int	connect_to_server(char *hostname)
 {
 	int					sock;
-	struct sockaddr_in	*server;
+	struct sockaddr		*server;
 
 	sock = socket(AF_INET, SOCK_STREAM, 0);
 	// config as a stream socket with connection through IPv4
