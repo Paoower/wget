ca92ef6596b1578484bf398edf0c70feb39173e2
Adrien DEBADIER <adriendebadier@hotmail.com>
2024-09-11 12:33:42 +0200

    fix error when packet were smaller than expected

--- src/get_file_from_host/http_header.c
+++ src/get_file_from_host/http_header.c
@@ -34,31 +34,32 @@ struct header_data	*skip_htpp_header(int sock,
 	char				*header_end;
 	char				*response_merged;
 	char				*new_response_merged;
-	int					i;
 	struct header_data	*header_data;
+	int					header_size;
+	char				*header_finish_pattern;
 
 	response_merged = NULL;
-	i = 1;
+	header_size = 0;
+	header_finish_pattern = "\r\n\r\n";
 	while ((*received = recv(sock, response, REQUEST_BUFFER_SIZE, 0)) > 0) {
-		new_response_merged = realloc(response_merged, REQUEST_BUFFER_SIZE * i);
+		header_size += *received;
+		new_response_merged = realloc(response_merged, header_size);
 		if (!new_response_merged) {
 			perror("Memory reallocation failed");
 			free(response_merged);
 			return NULL;
 		}
 		response_merged = new_response_merged;
-		memset(response_merged + REQUEST_BUFFER_SIZE * (i - 1),
-													0, REQUEST_BUFFER_SIZE);
+		memset(response_merged + header_size - *received, 0, *received);
 		strncat(response_merged, response, *received);
-		header_end = strstr(response_merged, "\r\n\r\n");
+		header_end = strstr(response_merged, header_finish_pattern);
 		if (header_end) {
-			*remaining_data_len = response_merged + REQUEST_BUFFER_SIZE * i
-															- (header_end + 4);
+			*remaining_data_len = response_merged + header_size
+								- (header_end + strlen(header_finish_pattern));
 			header_data = fill_http_data(response_merged);
 			free(response_merged);
 			return header_data;
 		}
-		i++;
 	}
 	free(response_merged);
 	if (*received < 0) {
