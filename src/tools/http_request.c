fb9ca59bde1a679cf19165e060e4867eefe466c4
Adrien DEBADIER <adriendebadier@hotmail.com>
2024-09-23 09:36:29 +0200

    fix case sensitive of http header data

--- src/tools/http_request.c
+++ src/tools/http_request.c
@@ -49,7 +49,7 @@ char	*get_http_response_info(const char *http_response,
 	while (lines[i]) {
 		tokens = split(lines[i], spliter);
 		if (tokens) {
-			if (tokens[0] && strcmp(tokens[0], key) == 0) {
+			if (tokens[0] && strcasecmp(tokens[0], key) == 0) {
 				result = strdup(lines[i] + strlen(tokens[0]) + strlen(spliter));
 				free_char_tab(tokens);
 				free_char_tab(lines);
